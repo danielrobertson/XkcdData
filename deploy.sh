2116d1c9bdc7e6f2dd8e4bbb268c73569bde6977
Daniel Robertson <danielrobertson733@gmail.com>
2016-05-03 17:04:03 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 /usr/local/apache-tomcat-8.0.33/bin/shutdown.sh
-rm /usr/local/apache-tomcat-8.0.33/webapps/JerseyTemplate*
+rm -rf /usr/local/apache-tomcat-8.0.33/webapps/xkcddata*
 cp build/libs/xkcddata.war /usr/local/apache-tomcat-8.0.33/webapps/
 /usr/local/apache-tomcat-8.0.33/bin/startup.sh # sometimes requires sudo

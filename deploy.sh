eff826d4f1e49b873c7fccc9c4fe38c5d8bd8984
danielrobertson <danielrobertson733@gmail.com>
2016-05-03 20:44:25 -0500

    catalina home variable

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-/usr/local/apache-tomcat-8.0.33/bin/shutdown.sh
-rm -rf /usr/local/apache-tomcat-8.0.33/webapps/xkcddata*
-cp build/libs/xkcddata.war /usr/local/apache-tomcat-8.0.33/webapps/
-/usr/local/apache-tomcat-8.0.33/bin/startup.sh # sometimes requires sudo
+CATALINA_HOME="/usr/local/apache-tomcat-8.0.33"
+
+$CATALINA_HOME/bin/shutdown.sh
+rm -rf $CATALINA_HOME/webapps/xkcddata*
+cp build/libs/xkcddata.war $CATALINA_HOME/webapps/
+$CATALINA_HOME/bin/startup.sh # sometimes requires sudo

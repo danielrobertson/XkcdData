c022ed1056a3ec8cfd707b318ddbe7e488c53bf5
danielrobertson <danielrobertson733@gmail.com>
2016-05-03 21:25:28 -0500

    add load api, updated readme

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # XkcdData
 A RESTful wrapper for Xkcd comics that pulls data from [Xkcd](http://xkcd.com) and exposes it as a REST API.
 
-This project is in development. 
+This project is in development.
 
-## Developer Environment Setup 
+## Usage
+Search for a relevant Xkcd comic
+/xkcddata/api/relevant/query/earth
+
+Load Xkcd comics in range N to M inclusive
+/xkcddata/api/load/begin/N/end/M
+
+## Developer Environment Setup
 
 ### Prerequisites
 - Java 8 
@@ -19,17 +26,11 @@ This project is in development.
 - Create a new schema   
 `create table xkcd (number BIGINT not null, title VARCHAR(255), image_url VARCHAR(255), transcript TEXT, alt TEXT, primary key (number));`
 
-## Build and Run 
-### ETL tool 
-Build  
-`gradle build`  
-
-Run  
-`gradle loadXkcdData -PRange="['40', '50']"` to load Xkcd comics numbers 40 through 50 inclusive into the database 
-### API 
+## Build and Run
 Build  
 `gradle build`  
 `gradle war`  
 
 Deploy  
-`sudo gradle deployTomcat`  
+`sudo gradle deployTomcat`
+
--- src/main/java/resources/XkcdDataResource.java
+++ src/main/java/resources/XkcdDataResource.java
@@ -16,12 +16,27 @@ public class XkcdDataResource {
      * @param searchTerm String - a search term
      * @return Response - serialized Xkcd bean json
      */
-    @Path("relevant/{search-term}")
+    @Path("relevant/query/{query}")
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getRelevantXkcd(@PathParam("search-term") final String searchTerm) {
-        Object response = "answer: " + searchTerm;
+    public Response getRelevantXkcd(@PathParam("query") final String searchTerm) {
+        Object response = "You searched for " + searchTerm;
         return Response.status(200).entity(response).type("text/plain").build(); // todo serialize MediaType.APPLICATION_JSON
+    }
+
+    /**
+     * Loads Xkcd comics into the database in given range, inclusive
+     * @param begin Integer - Xkcd number to begin loading, inclusive
+     * @param end Integer - Xkcd number to end loading, inclusive
+     * @return Response - Confirmation message
+     */
+    @Path("load/begin/{begin}/end/{end}")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response loadXkcdComicsInRange(@PathParam("begin") final Integer begin, @PathParam("begin") final Integer end) {
+        Object response = "Loading comics in range " + begin + " : " + end;
+        return Response.status(200).entity(response).type("text/plain").build();
     }
 }

7b3aac8bb371a31492ceb7ca420852259734c9de
danielrobertson <danielrobertson733@gmail.com>
2016-05-06 21:56:48 -0500

    connected API to database, having trouble with Tomcat making db connection

--- src/main/java/commands/RelevantXkcdCommand.java
+++ src/main/java/commands/RelevantXkcdCommand.java
@@ -2,6 +2,8 @@ package commands;
 
 import beans.Xkcd;
 import data.XkcdDao;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import search.RelevanceCalculator;
 
 import java.util.*;
@@ -10,6 +12,8 @@ import java.util.*;
  * Searches all Xkcd comics for the most relevant to a search term
  */
 public class RelevantXkcdCommand {
+    // logger
+    private static Log log = LogFactory.getLog(RelevantXkcdCommand.class);
 
     /**
      * Determines and returns the most relevant Xkcd comic for the given search term
@@ -17,12 +21,13 @@ public class RelevantXkcdCommand {
      * @param searchTerm String - a search term
      * @return Xkcd bean representing the most relevant Xkcd
      */
-    public static Xkcd getRelevantXkcd(String searchTerm) {
+    public Xkcd getRelevantXkcd(String searchTerm) {
         Map<Xkcd, Double> confidenceMap = new HashMap<>();
         RelevanceCalculator relevanceCalculator = new RelevanceCalculator();
 
         XkcdDao dao = new XkcdDao();
         List<Xkcd> xkcdList = dao.retrieveAllComics();
+        log.debug("xkcd list count - " + xkcdList.size());
 
         // all transcripts necessary for the inverse document frequency calculation
         List<List<String>> allTranscripts = new ArrayList<>();
@@ -30,6 +35,7 @@ public class RelevantXkcdCommand {
             List<String> words = Arrays.asList(x.getTranscript().replaceAll("[\\W&&[^\\s]]", "").split("\\W+"));
             allTranscripts.add(words);
         }
+        log.debug("words count - " + allTranscripts.size());
 
         for (Xkcd x : xkcdList) {
             List<String> words = Arrays.asList(x.getTranscript().replaceAll("[\\W&&[^\\s]]", "").split("\\W+"));
@@ -37,14 +43,10 @@ public class RelevantXkcdCommand {
             confidenceMap.put(x, confidence);
         }
 
+        log.info("Confidence map - \n" + confidenceMap.toString());
+
         // xkcd with the highest confidence is our winner
         Xkcd mostRelevant = Collections.max(confidenceMap.entrySet(), (x1, x2) -> x1.getValue().compareTo(x2.getValue())).getKey();
         return mostRelevant;
     }
-
-    // testing only - TODO remove main after creating the API
-    public static void main(String[] args) {
-        System.out.println("Most relevant is \n" + getRelevantXkcd("earth"));
-    }
-
 }
--- src/main/java/resources/XkcdDataResource.java
+++ src/main/java/resources/XkcdDataResource.java
@@ -1,5 +1,9 @@
 package resources;
 
+import commands.RelevantXkcdCommand;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -9,6 +13,8 @@ import javax.ws.rs.core.Response;
  */
 @Path("/")
 public class XkcdDataResource {
+    // logger
+    private static Log log = LogFactory.getLog(XkcdDataResource.class);
 
     /**
      * Finds and returns a relevant Xkcd comic for the given seach term
@@ -21,14 +27,19 @@ public class XkcdDataResource {
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
     public Response getRelevantXkcd(@PathParam("query") final String searchTerm) {
-        Object response = "You searched for " + searchTerm;
-        return Response.status(200).entity(response).type("text/plain").build(); // todo serialize MediaType.APPLICATION_JSON
+        log.debug("Search query - " + searchTerm);
+        RelevantXkcdCommand cmd = new RelevantXkcdCommand();
+        Object resultingXkcd = cmd.getRelevantXkcd(searchTerm);
+
+        log.debug("Resulting Xkcd - " + resultingXkcd);
+        return Response.status(200).entity(resultingXkcd).type(MediaType.APPLICATION_JSON).build();
     }
 
     /**
      * Loads Xkcd comics into the database in given range, inclusive
+     *
      * @param begin Integer - Xkcd number to begin loading, inclusive
-     * @param end Integer - Xkcd number to end loading, inclusive
+     * @param end   Integer - Xkcd number to end loading, inclusive
      * @return Response - Confirmation message
      */
     @Path("load/begin/{begin}/end/{end}")

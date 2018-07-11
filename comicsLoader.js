2d4c5e2a1e3bb646563724be25b69101b9285bf7
Daniel Robertson <danielrobertson733@gmail.com>
2018-07-11 13:15:00 -0500

    not using Firebase anymore, output JSON for upload to Appbase

--- comicsLoader.js
+++ comicsLoader.js
@@ -1,39 +1,24 @@
 const axios = require("axios");
-const firebase = require("firebase-admin");
-const serviceAccount = require("./firebase-config.json");
 
-// dataase config
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://relevant-xkcd-comics.firebaseio.com"
-});
-
-const db = firebase.firestore();
-
-// initialize database by loading comics
-let start; // 1
-let end; // 2008
+let start = 404; // 1
+let end = 2017; // 2008
 loadComicsInRange(start, end);
 
 /*
- * Loads comics into Firestore database from range start to end (inclusive)
+ * Get comics JSON from range start to end (inclusive)
  * according to the property `num` from the Xkcd API
  */
 function loadComicsInRange(start, end) {
   for (let id = start; id <= end; id++) {
     const url = `https://xkcd.com/${id}/info.0.json`;
-    console.log("Url - " + url);
     axios
       .get(url)
       .then(function(response) {
         const comic = response.data;
-        const docRef = db.collection("comics").doc(comic.title);
-        docRef.set(comic);
-        console.log("success, id - " + id);
+        console.log(JSON.stringify(comic) + ",");
       })
-      .catch(function(error) {
-        console.log("error, id - " + id);
-        console.log(error);
+      .catch(err => {
+        console.log(err);
       });
   }
 }
